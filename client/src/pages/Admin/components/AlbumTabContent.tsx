82e814a5359fb95bad4226e68b86575a0410d27d
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2025-01-18 15:05:25 +0530

    Admin Page & Add ALbums

--- client/src/pages/Admin/components/AlbumTabContent.tsx
+++ client/src/pages/Admin/components/AlbumTabContent.tsx
@@ -1,8 +1,29 @@
+import { CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Library } from "lucide-react"
+import { Button } from '@/components/ui/button';
+import AlbumsTable from "./AlbumsTable";
 
 
 const AlbumTabContent = () => {
   return (
-    <div>AlbumTabContent</div>
+    <div className="bg-zinc-800/50 border-zinc-700/50">
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Library className="size-5 text-violet-500"/>
+              Album Library
+            </CardTitle>
+            <CardDescription>Manage your music albums</CardDescription>
+          </div>
+          <Button>+</Button>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <AlbumsTable/>
+      </CardContent>
+    </div>
   )
 }
 
